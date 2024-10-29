691da3f2f1985b0b75fa211e2d7a2591e59f8dd2
vishalmeti <vishalmeti9@gmail.com>
2024-10-30 01:54:55 +0530

    enabling ai to have prev chat access/memory

--- gemini-clone/src/config/gemini.js
+++ gemini-clone/src/config/gemini.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
 
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
-  const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
-  const genAI = new GoogleGenerativeAI(apiKey);
   
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
   });
   
+  const chatHistory = [];
   const generationConfig = {
     temperature: 1,
     topP: 0.95,
@@ -15,15 +17,36 @@ import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/ge
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
   };
-  
+  const getHistory = () => {
+    return chatHistory
+   };
+
+   const updateHistory = (obj) =>{
+      chatHistory.push(obj);
+   }
   async function run(prompt) {
+
     const chatSession = model.startChat({
       generationConfig,
-      history: [],
+      history: getHistory(),
     });
-  
+    
     const result = await chatSession.sendMessage(prompt);
     console.log(result.response.text());
+    
+    let userObj= {
+      role: "user",
+      parts: [{ text: prompt}],
+    };
+
+    let aiObj={
+      role: "model",
+      parts: [{ text: result.response.text()}],
+    };
+
+    updateHistory(userObj);
+    updateHistory(aiObj);
+
     return result.response.text()
   }
   
