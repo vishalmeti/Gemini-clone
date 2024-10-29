626a7e33928658c6fdb6fcdc5b6572e10ca1bb8c
vishalmeti <vishalmeti9@gmail.com>
2024-10-30 00:14:25 +0530

    Refactor Main and Sidebar components

--- gemini-clone/src/components/Main/Main.jsx
+++ gemini-clone/src/components/Main/Main.jsx
@@ -34,6 +34,20 @@ const Main = () => {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder="Enter a promt here" />
+                    <div className="">
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className="bottom-info">
+                     Gemini may display inaccurate info, including about people, so double-check before using it to make decisions.
+                </p>
+            </div>
         </div>
     </div>
   )
